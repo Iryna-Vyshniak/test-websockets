5217440913f5abc6b6feaf8fbab7c109e4c11e7c
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-12-08 23:15:34 +0200

    refactor function to edit name in the database

--- server/controllers/editNameCard.js
+++ server/controllers/editNameCard.js
@@ -1,10 +1,11 @@
 const db = require('../db');
 
 const editNameCard = data => {
+  // console.log('data EDIT: ', data);
   return new Promise((resolve, reject) => {
-    const id = data.id;
+    const id = data[0].id;
     const sql = `UPDATE info SET name = COALESCE(?, name) WHERE id = ?;`;
-    const params = [data.name, id];
+    const params = [data[0].name, id];
 
     db.run(sql, params, function (err) {
       if (err) {
