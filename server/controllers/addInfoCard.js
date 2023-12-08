79f5356fe6d67b27884ec4605ce60f25cbb35d37
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-12-08 23:15:04 +0200

    refactor function to insert information into the database

--- server/controllers/addInfoCard.js
+++ server/controllers/addInfoCard.js
@@ -1,29 +1,33 @@
 const db = require('../db');
 
-const addInfoCard = data => {
-  return new Promise((resolve, reject) => {
-    const errors = [];
-
+const addInfoCard = async data => {
+  try {
+    // Validation
     if (!data.name || !data.orgname) {
-      errors.push('No name or orgname specified');
-      reject(errors.join(','));
+      throw new Error('No name or orgname specified');
     }
 
+    // SQL query and parameters
     const sql = 'INSERT INTO info (name, orgname, datecreate) VALUES (?, ?, CURRENT_DATE);';
     const params = [data.name, data.orgname];
 
+    // Execute the query
     db.run(sql, params, function (err) {
       if (err) {
-        reject('Error inserting data into the database');
+        new Error('Error inserting data into the database');
       }
-
-      resolve({
-        message: 'success',
-        data,
-        id: this.lastID
-      });
+      console.log(`A row has been inserted`);
     });
-  });
+
+    // Return success message, data, and ID
+    return {
+      message: 'success',
+      data,
+      id: this.lastID
+    };
+  } catch (error) {
+    throw error;
+  }
 };
 
 module.exports = addInfoCard;
