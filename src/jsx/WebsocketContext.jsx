e72d7de6e2f2699d07f14fe8a00bfa99798340d8
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2023-12-10 21:58:55 +0200

    upd websocket context

--- src/jsx/WebsocketContext.jsx
+++ src/jsx/WebsocketContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useContext, useEffect, useState } from 'react';
-import { connectWebSocket, on, sendWebSocketMessage, closeWebSocket } from '../WebSocketClient';
+import { closeWebSocket } from '../WebSocketClient';
+// import { connectWebSocket, on, sendWebSocketMessage, closeWebSocket } from '../WebSocketClient';
 
 const WebsocketContext = createContext();
 
@@ -12,28 +13,51 @@ export const useWebsocket = () => {
   return context;
 };
 
-const WebsocketProvider = ({ url, children }) => {
-  const [ws, setWs] = useState(null);
+const WebsocketProvider = ({ ws, children }) => {
+  // const [ws, setWs] = useState(null);
 
-  useEffect(() => {
-    const websocket = connectWebSocket(url);
+  // useEffect(() => {
+  //   const websocket = connectWebSocket(url);
 
-    setWs(websocket);
+  //   setWs(websocket);
 
-    return () => {
-      closeWebSocket(websocket);
-    };
-  }, [url]);
+  //   return () => {
+  //     closeWebSocket(websocket);
+  //   };
+  // }, [url]);
 
   const contextValue = {
     ws,
-    connectWebSocket,
-    on,
-    sendWebSocketMessage,
+    //connectWebSocket,
+    // on,
+    // sendWebSocketMessage,
     closeWebSocket
   };
 
   return <WebsocketContext.Provider value={contextValue}>{children}</WebsocketContext.Provider>;
 };
+// const WebsocketProvider = ({ url, children }) => {
+//   const [ws, setWs] = useState(null);
+
+//   useEffect(() => {
+//     const websocket = connectWebSocket(url);
+
+//     setWs(websocket);
+
+//     return () => {
+//       closeWebSocket(websocket);
+//     };
+//   }, [url]);
+
+//   const contextValue = {
+//     ws,
+//     connectWebSocket,
+//     // on,
+//     // sendWebSocketMessage,
+//     closeWebSocket
+//   };
+
+//   return <WebsocketContext.Provider value={contextValue}>{children}</WebsocketContext.Provider>;
+// };
 
 export default WebsocketProvider;
