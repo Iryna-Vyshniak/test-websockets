53b3c049f9fea1dd242db19b5545410d6e0178dd
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2023-12-11 22:28:11 +0200

    refactor  onChange and handleSubmit

--- src/jsx/components/Form/AddForm.jsx
+++ src/jsx/components/Form/AddForm.jsx
@@ -1,59 +1,33 @@
 import React, { useState } from 'react';
-import { toast } from 'react-toastify';
-import { notifyOptions } from '../../../shared/utils/notify';
 
 import styles from './Form.module.scss';
 import Input from './Input/Input';
 import Button from '../Button/Button';
 import fields from './fields';
 import initialState from './initialState';
-import { sendWebSocketMessage } from '../../../WebSocketClient';
 
-const AddForm = ({ onClose }) => {
+const AddForm = ({ onClose, onData }) => {
   const [data, setData] = useState(initialState);
-  const [websocketReady, setWebsocketReady] = useState(false);
 
   const { name, orgname, datecreate } = data;
 
-  const onChangeName = event => {
-    setData({
-      ...data,
-      name: event.target.value
-    });
-  };
-
-  const onChangeOrg = event => {
-    setData({
-      ...data,
-      orgname: event.target.value
-    });
-  };
-
-  const onChangeDate = event => {
-    setData({
-      ...data,
-      datecreate: event.target.value
-    });
+  const onChange = ({ target }) => {
+    const { name: inputName, value, checked, type } = target;
+    const newValue = type === 'checkbox' ? checked : value;
+    setData(prev => ({
+      ...prev,
+      [inputName]: newValue
+    }));
   };
 
   const isInputFieldEmpty = () => {
     return data.name === '' || data.orgname === '' || data.datecreate === '';
   };
 
-  const handleSubmit = async () => {
-    try {
-      await sendWebSocketMessage('addInfoCard', data, receivedData => {
-        // Check if we have already received data
-        if (!data) {
-          setData(receivedData.data);
-          setWebsocketReady(true);
-          onClose();
-        }
-      });
-    } catch (error) {
-      console.error(error);
-      toast.error(`Info didn't create`, notifyOptions);
-    }
+  const handleSubmit = async e => {
+    e.preventDefault();
+    onData(data);
+    onClose();
   };
 
   const handleReset = e => {
@@ -65,11 +39,11 @@ const AddForm = ({ onClose }) => {
   return (
     <div className={styles.wrapper}>
       <form onSubmit={handleSubmit} className={styles.form} action="">
-        <Input value={name} onChange={onChangeName} {...fields.name} />
-        <Input value={orgname} onChange={onChangeOrg} {...fields.orgname} />
-        <Input value={datecreate} onChange={onChangeDate} {...fields.datecreate} />
+        <Input value={name} onChange={onChange} {...fields.name} />
+        <Input value={orgname} onChange={onChange} {...fields.orgname} />
+        <Input value={datecreate} onChange={onChange} {...fields.datecreate} />
         <div className={styles.wrappers}>
-          <Button type="submit" text="Ok" disabled={!websocketReady && isInputFieldEmpty()} />
+          <Button type="submit" text="Ok" disabled={isInputFieldEmpty()} />
           <Button type="reset" text="Cancel" onClick={handleReset} />
         </div>
       </form>
