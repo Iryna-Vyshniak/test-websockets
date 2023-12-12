693e7bc4eba3b058e7bb656532e376e5daad4bd5
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2023-12-12 20:52:06 +0200

    Uncommented and cleaned up code related to websockets, removed unused imports

--- src/jsx/pages/DesktopPage/DesktopPage.jsx
+++ src/jsx/pages/DesktopPage/DesktopPage.jsx
@@ -1,19 +1,23 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import Card from '../../components/Card/Card';
 import Button from '../../components/Button/Button';
 import Modal from '../../components/Modal/Modal';
 import AddForm from '../../components/Form/AddForm';
 
-import Spinner from '../../components/Spinner/Spinner';
-import { sendWebSocketMessage } from '../../../WebSocketClient';
+// import Spinner from '../../components/Spinner/Spinner';
+import { useWebsocket } from '../../WebsocketContext';
 
-const DesktopPage = () => {
+const DesktopPage = ({ info, onData }) => {
   const [showModal, setShowModal] = useState(false);
-  const [data, setData] = useState();
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const [websocketReady, setWebsocketReady] = useState(false);
+  // const [info, setInfo] = useState({});
+
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState(false);
+
+  const { ws } = useWebsocket();
+  // const { sendWebSocketMessage, ws, on } = useWebsocket();
+  console.log(ws);
 
   const onShowModal = () => {
     setShowModal(true);
@@ -23,45 +27,67 @@ const DesktopPage = () => {
     setShowModal(false);
   };
 
-  useEffect(() => {
-    (async () => {
-      try {
-        setIsLoading(true);
-        setError(false);
+  // const message = {
+  //   event: 'getAllInfo'
+  // };
 
-        // Send a message to the WebSocket server to get all info
-        sendWebSocketMessage('getAllInfo', null, receivedData => {
-          // Check if we have already received data
-          if (!data) {
-            setData(receivedData.data);
-          }
-          setWebsocketReady(true);
-          setIsLoading(false);
-        });
-      } catch (error) {
-        setError(true);
-        console.log(error.message);
-      } finally {
-        setIsLoading(false);
-      }
-    })();
-  }, [data, websocketReady]);
+  // ws.onopen = () => {
+  //   ws.send(JSON.stringify(message));
+  // };
 
+  // ws.onmessage = e => {
+  //   const message = JSON.parse(e.data);
+  //   setInfo(message.value);
+  // };
+
+  // useEffect(() => {
+  //   (async () => {
+  //     try {
+  //       setIsLoading(true);
+  //       setError(false);
+
+  //       if (ws.readyState === WebSocket.OPEN) {
+  //         await on('getAllInfo', data => {
+  //           console.log('Received all info:', data);
+  //           console.log('Type of data:', typeof data);
+  //         });
+
+  //         await sendWebSocketMessage('getAllInfo', null, receivedData => {
+  //           console.log('Data before setting:', receivedData);
+
+  //           setInfo(receivedData);
+  //           console.log('Data after setting:', receivedData);
+
+  //           setIsLoading(false);
+  //         });
+
+  //         setIsLoading(false);
+  //       }
+  //     } catch (error) {
+  //       setError(true);
+  //       console.log(error.message);
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   })();
+  // }, [on, sendWebSocketMessage, ws]);
+
+  console.log('INFO', info);
   return (
     <>
       {showModal && (
         <Modal close={onCloseModal}>
-          <AddForm onClose={onCloseModal} />
+          <AddForm onClose={onCloseModal} onData={onData}/>
         </Modal>
       )}
-      <Button text="Add" onClick={onShowModal} disabled={!websocketReady} />
-      {isLoading && <Spinner />}
+      <Button text="Add" onClick={onShowModal} disabled={ws?.readyState !== WebSocket.OPEN} />
+      {/* {isLoading && <Spinner />}
       {error && <div>Opps, error... Please, wait or update page</div>}
-      {!error && !isLoading && data?.length > 0 && (
-        <>
-          <Card data={data} />
-        </>
-      )}
+      {!error && !isLoading && info?.length > 0 && ( */}
+      <>
+        <Card data={info} />
+      </>
+      {/* )} */}
     </>
   );
 };
