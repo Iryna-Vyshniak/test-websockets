0c8e170a11f8597748d6f3d59db5d703c9712437
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2023-12-12 20:54:12 +0200

    upd websocket provider

--- src/index.js
+++ src/index.js
@@ -5,14 +5,18 @@ import './scss/main.scss';
 
 import reportWebVitals from './reportWebVitals';
 import App from './jsx/App';
+import WebsocketProvider from './jsx/WebsocketContext';
 
-// connectWebSocket('ws://localhost:5000/test');
+const ws = new WebSocket('ws://localhost:5000');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <WebsocketProvider ws={ws}>
+    {/* <WebsocketProvider url={'ws://localhost:5000/test'}> */}
     <App />
-  </React.StrictMode>
+  </WebsocketProvider>
+  // </React.StrictMode>
 );
 
 reportWebVitals();
